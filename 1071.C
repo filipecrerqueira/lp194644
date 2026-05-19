03bd5216e0ebc329e0722ae2235f5e51e14c32a6
filipecrerqueira <filipe.cerqueira@unifesp.br>
2026-05-19 20:38:17 -0300

    Update 1071.c

--- 1071.C
+++ 1071.C
@@ -1,52 +1,42 @@
 /* --------------------------------------------------------------------------
-Disciplina  : Lógica de Programação, turma IB, 194644
+Disciplina  : LC3gica de Programacao, turma IB, 194644
 Linguagem   : C
-Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 1071
-Data        : 17/04/2026
-Objetivo    : Laço for
-Aprendizado : usar o for e condicionais
+Plataforma  : https://judge.beecrowd.com/pt/search1017
+ExercC-cio   : 1017
+Data        : 19/05/2026
+Objetivo    : Fucoes
+Aprendizado : desvendar a lei de formacao e montar a matriz
 -------------------------------------------------------------------------- */
 
-#include<stdio.h>
+#include <stdio.h>
 
- int main(){
- 
- 
-    int X,Y,s,i,maior,menor;
+float calcularLitros(int tempo, int velocidade){
     
-    X=0;
-    Y=0;
-    s=0;
-    scanf("%d %d", &X, &Y);
+     float distancia = velocidade*tempo;
+     
+     float litros = distancia/12;
+     
+     return litros;
+     
+}
+
+int main(){
     
-    if (Y<X){
-        menor=Y; 
-        maior=X;
-        
-        
-    } else{
-        menor=X;
-        maior=Y;
-      }
+    int tempo, velocidade;
+    float litros;
     
-    for(i=menor;i<maior;i++){
-        if(i%2!=0 && i!=menor){
-            
-         s+=i;
-        }
-        
-    }    
-        
-        
-        
-    printf("%d\n",s);
-        
+    scanf("%d", &tempo);
+    scanf("%d", &velocidade);
+    
+    litros = calcularLitros(tempo, velocidade);
+    
+    printf("%.3f\n", litros);
     
     
     
-        return 0;          
-  }
     
+
+return 0;        
     
-    
+
+}
