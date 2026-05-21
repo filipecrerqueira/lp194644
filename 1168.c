5f3813460f2fc30ee886f2721a50ac4c31fe43d7
filipecrerqueira <filipe.cerqueira@unifesp.br>
2026-05-21 15:08:47 -0300

    Update 1168.c

--- 1168.c
+++ 1168.c
@@ -11,7 +11,7 @@ Aprendizado : utilizar informacoes salvas no vetor
 #include <stdio.h>
 
 int main() {
-	int led[] = {6,2,5,5,4,5,6,3,7,6};
+	
 	int N;
 	char num[110];
 	int total = 0;
@@ -24,10 +24,41 @@ int main() {
 		scanf("%s", num);
 
 		for(int z=0; num[z] != '\0'; z++) {
+		    
 
-			int digito = (num[z]-'0');
-
-			total += led[digito];
+		    switch(num[z]){
+		        case '0':
+		            total+= 6;
+		            break;
+		        case '1':
+		            total+= 2;
+		            break;
+		        case '2':
+		            total+= 5;
+		            break;
+		        case '3':
+		            total += 5;
+		            break;
+		        case '4':
+		            total += 4;
+		            break;
+		        case '5':
+		            total += 5;
+		            break;
+		        case '6':
+		            total += 6;
+		            break;
+		        case '7':
+		            total += 3;
+		            break;
+		        case '8':
+		            total += 7;
+		            break;        
+		        case '9':
+		            total += 6;
+		            break;
+		    }    
+		
 
 
 		}
@@ -42,4 +73,3 @@ int main() {
 
 	return 0;
 }
-
