dba557b5bdf7b43b1aa809d2b12daac01877858c
filipecrerqueira <filipe.cerqueira@unifesp.br>
2026-06-16 17:05:04 -0300

    Create 1030.c (ainda em curso)

---
+++ 1030.c
@@ -0,0 +1,46 @@
+/* --------------------------------------------------------------------------
+Disciplina  : LC3gica de ProgramaC'C#o, turma IB, 194644
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/search1030
+ExercC-cio   : 1030
+Data        : 16/06/2026
+Objetivo    : Josephos
+Aprendizado : logica circular de vetores
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main(){
+
+    int casos;
+
+    scanf("%d", casos);
+
+    for (size_t i = 0; i < casos; i++)
+    {
+
+        int n;
+
+        scanf("%d", &n);
+
+        int pessoas[n];
+
+        int pulos;
+        
+        scanf("%d", &pulos);
+
+        for (size_t i = 0; i < pessoas; i++)
+        {
+            pessoas[i] = 1;
+        }
+    
+        for (size_t i = 0; i < pessoas; i++)
+        {
+            int pulo=()
+        }
+        
+        
+
+
+    }
+
+}
