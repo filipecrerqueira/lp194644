42c5053b8efe12227cd1dc0df27b8c925b417bb0
filipecrerqueira <filipe.cerqueira@unifesp.br>
2026-06-21 18:43:45 -0300

    Update 1383.c

--- 1383.c
+++ 1383.c
@@ -3,9 +3,9 @@
 
 int verificacao_linhas(int sudoku[9][9], int linha);
 int verificacao_colunas(int sudoku[9][9], int coluna);
-int verrificacao_bloco(int sudoku[9][9], int linha_inicio, int coluna_inicio);
+int verificacao_bloco(int sudoku[9][9], int linha_inicio, int coluna_inicio);
 
-int verificacao_linhas(int sudoku[9][9], int coluna)
+int verificacao_linhas(int sudoku[9][9], int linha)
 {
 
     int verif[9];
@@ -18,20 +18,20 @@ int verificacao_linhas(int sudoku[9][9], int coluna)
     for (size_t i = 0; i < 9; i++)
     {
 
-    
-        if (verif[sudoku[i][coluna]-1] == 0) //primeira leitura que verfica se ta ok, se tiver ok soma mais um e confirma a 'leitura'
+        if (verif[sudoku[linha][i] - 1] == 0) // primeira leitura que verfica se ta ok, se tiver ok soma mais um e confirma a 'leitura'
         {
 
-            verif[sudoku[i][coluna]-1] += 1;
+            verif[sudoku[linha][i] - 1] += 1;
         }
         else
         {
             return 0;
         }
     }
-
-
+    return 1;
 }
+
+
 int verificacao_colunas(int sudoku[9][9], int coluna)
 {
 
@@ -45,48 +45,113 @@ int verificacao_colunas(int sudoku[9][9], int coluna)
     for (size_t i = 0; i < 9; i++)
     {
 
-    
-        if (verif[sudoku[i][coluna]-1] == 0)
+        if (verif[sudoku[i][coluna] - 1] == 0)
         {
 
-            verif[sudoku[i][coluna]-1] += 1;
+            verif[sudoku[i][coluna] - 1] += 1;
         }
         else
         {
             return 0;
         }
     }
-
-}    
-int verrificacao_bloco(int sudoku[9][9], int linha_inicio, int coluna_inicio ) // precisa verificar o bloco com isso tem 9 blocos e precisa separar cada inicio deles  
-{
-
-    //precisa montar algo aqui para mudar sempre a lina de inico e/ou coluna
-
-
-    for (int i = linha_inicio; i < linha_inicio + 3; i++)
-    {   for (int j = coluna_inicio; i < coluna_inicio + 3; i++)
-    {
-        
-
-
-    }
-    
-        
-    }
-    
-
-
+    return 1;
 }
 
+
+int verificacao_bloco(int sudoku[9][9], int linha_inicio, int coluna_inicio) // precisa verificar o bloco com isso tem 9 blocos e precisa separar cada inicio deles
+{
+    int verif[9];
+
+    for (size_t i = 0; i < 9; i++)
+    {
+        verif[i] = 0;
+    }
+
+    for (int i = linha_inicio; i < linha_inicio + 3; i++)
+    {
+        for (int j = coluna_inicio; j < coluna_inicio + 3; j++)
+        {
+
+            int numero = sudoku[i][j]; // aqui ro armazenando o valor das casas para o verif ler
+
+                if (verif[numero - 1] == 0) // começa a verificacao, ja tem um loop para garantir que vai verif tudo
+            {
+
+                verif[numero - 1] += 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
 int main()
 {
-
+    int sudoku[9][9];
     int casos;
+    int linha_inicio , coluna_inicio ;
 
     scanf("%d", &casos);
 
-    for (int i = 0; i < casos; i++)
+    for (int c = 0; c < casos; c++)
     {
+        for (int i = 0; i < 9; i++)
+        {   for (int j = 0; j < 9; j++)
+        {
+                scanf("%d", &sudoku[i][j]);
+
+        }
+        }
+        
+        int valido = 1;
+
+        for (size_t z = 0; z < 9; z++) // laco para comecar as verif, com um parametro fixo e outro variavel como ja passado para a função
+        {
+           if( verificacao_colunas(sudoku, z) == 0){
+
+                valido = 0;
+
+           }
+            
+           if( verificacao_linhas(sudoku, z) == 0){
+
+                valido = 0;
+           }
+
+        }
+        
+        
+
+
+
+
+        for ( linha_inicio = 0; linha_inicio < 9; linha_inicio += 3)
+        {
+            for (coluna_inicio = 0; coluna_inicio < 9; coluna_inicio += 3)
+            {
+
+               if( verificacao_bloco( sudoku, linha_inicio, coluna_inicio) == 0){
+
+                valido = 0;
+
+               }
+
+            }
+        }
+    
+
+    if (valido == 0 )
+    {
+        printf("Instancia %d\nNAO\n\n", c + 1);
+    } else{
+
+        printf("Instancia %d\nSIM\n\n", c + 1);
     }
+    
+}
+
+return 0;
 }
